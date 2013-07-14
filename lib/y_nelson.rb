a8a8f6bf263eb81ab4676eff7ab89bbd87fe2488
boris <boris@iis.sinica.edu.tw>
2013-07-15 01:31:33 +0800

    splitting of dsl.rb from y_nelson.rb

--- lib/y_nelson.rb
+++ lib/y_nelson.rb
@@ -15,6 +15,7 @@ require_relative 'y_nelson/net'
 require_relative 'y_nelson/zz_point'
 require_relative 'y_nelson/dimension_point'
 require_relative 'y_nelson/manipulator'
+require_relative 'y_nelson/dsl'
 
 # YNelson is an implementation of a cross between Ted Nelson's Zz structure, and
 # a functional Petri net (FPN). The resulting data structure, which combines
@@ -59,89 +60,25 @@ module YNelson
     include YPetri::Workspace::PetriNetRelatedMethods
     include YPetri::Workspace::SimulationRelatedMethods
 
-    # Allows summoning DSL by 'include YNelson' (like YPetri does).
+    # Allows summoning YNelson::DSL by 'include YNelson' (like YPetri does).
     # 
-    def included receiver
-      # receiver.instance_variable_set :@NelsonManipulator, Manipulator.new
-      # puts "included in #{receiver}"
-      receiver.module_exec {
-        define_method :nelson_manipulator do
-          singleton_class.instance_variable_get :@NelsonManipulator or
-            ( puts "defining Manipulator for #{self} singleton class" if YNelson::DEBUG
-              singleton_class.instance_variable_set :@NelsonManipulator, Manipulator.new )
-        end
-      }
+    def included( receiver )
+      receiver.extend YPetri::DSL
+      receiver.delegate :y_nelson_manipulator, to: :class
     end
 
-    def dimensions; @dimensions end
+    def dimensions
+      @dimensions
+    end
   end # class << self
 
-  # Instance variables @Place, @Transition, @Net are expected by
-  # YPetri::Workspace module.
-  @Place, @Transition, @Net = self::Place, self::Transition, self::Net
+  # @Place, @Transition, @Net are expected by YPetri::Workspace module.
+  # 
+  @Place = self::Place
+  @Transition = self::Transition
+  @Net = self::Net
 
-  # Atypical call to #initialize in the course of module execution.
+  # Atypical #initialize call in the course of module execution.
+  # 
   initialize
-  
-  delegate( :workspace, to: :nelson_manipulator )
-
-  # Petri net aspect.
-  delegate( :Place, :Transition, :Net,
-            :place, :transition, :pl, :tr,
-            :places, :transitions, :nets,
-            :pp, :tt, :nn,
-            :net_point,
-            :net_selection,
-            :net, :ne,
-            :net_point_reset,
-            :net_point_set,
-            to: :nelson_manipulator )
-
-  # Simulation aspect.
-  delegate( :simulation_point, :ssc_point, :cc_point, :imc_point,
-            :simulation_selection, :ssc_selection,
-            :cc_selection, :imc_selection,
-            :simulations,
-            :clamp_collections,
-            :initial_marking_collections,
-            :simulation_settings_collections,
-            :clamp_collection_names, :cc_names,
-            :initial_marking_collection_names, :imc_names,
-            :simulation_settings_collection_names, :ssc_names,
-            :set_clamp_collection, :set_cc,
-            :set_initial_marking_collection, :set_imc,
-            :set_simulation_settings_collection, :set_ssc,
-            :new_timed_simulation,
-            :clamp_cc, :initial_marking_cc, :simulation_settings_cc,
-            :simulation_point_position,
-            :simulation,
-            :clamp_collection, :cc,
-            :initial_marking_collection, :imc,
-            :simulation_settings_collection, :ssc,
-            :clamp,
-            :initial_marking,
-            :set_step, :set_step_size,
-            :set_time, :set_target_time,
-            :set_sampling,
-            :set_simulation_method,
-            :new_timed_simulation,
-            :run!,
-            :print_recording,
-            :plot,
-            :plot_selected,
-            :plot_state,
-            :plot_flux,
-            to: :nelson_manipulator )
-
-  # Zz aspect.
-  delegate( :Dimension,
-            :ϝ,
-            :default_dimension,
-            :primary_point, :p1,
-            :secondary_point, :p2,
-            :primary_dimension_point, :d1,
-            :secondary_dimension_point, :d2,
-            :visualize,
-            :graphviz,
-            to: :nelson_manipulator )
 end
---
+++ lib/y_nelson/dsl.rb
@@ -0,0 +1,76 @@
+# -*- coding: undecided -*-
+module YNelson
+  # YNelson DSL.
+  # 
+  module DSL
+    def y_nelson_manipulator
+      @y_nelson_manipulator ||= Manipulator.new
+        .tap { puts "Defining Manipulator for #{self}" if YNelson::DEBUG }
+    end
+  end
+
+  delegate :workspace, to: :y_nelson_manipulator
+
+  # Petri net aspect.
+  delegate( :Place,
+            :Transition, :T, :A,
+            :Net,
+            :place, :transition, :pl, :tr,
+            :places, :transitions, :nets,
+            :pn, :tn, :nn,
+            :net_point,
+            :net_selection,
+            :net, :nnet,
+            :net_point_reset,
+            :net_point=,
+            to: :y_nelson_manipulator )
+
+  # Simulation aspect.
+  delegate( :simulation_point, :ssc_point, :cc_point, :imc_point,
+            :simulation_selection, :ssc_selection,
+            :cc_selection, :imc_selection,
+            :simulations,
+            :clamp_collections,
+            :initial_marking_collections,
+            :simulation_settings_collections,
+            :clamp_collection_names, :ncc,
+            :initial_marking_collection_names, :nimc,
+            :simulation_settings_collection_names, :nssc,
+            :set_clamp_collection, :set_cc,
+            :set_initial_marking_collection, :set_imc,
+            :set_simulation_settings_collection, :set_ssc,
+            :new_simulation,
+            :clamp_cc, :initial_marking_cc, :simulation_settings_cc,
+            :simulation_point_position,
+            :simulation,
+            :clamp_collection, :cc,
+            :initial_marking_collection, :imc,
+            :simulation_settings_collection, :ssc,
+            :clamp,
+            :initial_marking,
+            :set_step, :set_step_size,
+            :set_time, :set_target_time,
+            :set_sampling,
+            :set_simulation_method,
+            :new_timed_simulation,
+            :run!,
+            :print_recording,
+            :plot,
+            :plot_selected,
+            :plot_state,
+            :plot_flux,
+            to: :y_nelson_manipulator )
+
+
+  # Zz aspect.
+  delegate( :Dimension,
+            :ϝ,
+            :default_dimension,
+            :primary_point, :p1,
+            :secondary_point, :p2,
+            :primary_dimension_point, :d1,
+            :secondary_dimension_point, :d2,
+            :visualize,
+            :graphviz,
+            to: :nelson_manipulator )
+end
